7d22b0c2aa06cdc76563a3494eaaf70ae1c4a246
CliffracerX <thedeepseaowen@gmail.com>
2014-02-12 17:08:10 -0500

    Transparent block tweaks.
    Will making swimming pools using decoration water so much cooler.  :D

--- cliffracerx/mods/cliffiesdecor/src/DecorModBlockNoclip.java
+++ cliffracerx/mods/cliffiesdecor/src/DecorModBlockNoclip.java
@@ -5,11 +5,13 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class DecorModBlockNoclip extends Block
 {
     private String tex;
+    boolean localFlag=false;
     
     public DecorModBlockNoclip(int id, Material material, String texture)
     {
@@ -33,5 +35,16 @@ public class DecorModBlockNoclip extends Block
     public int getRenderBlockPass()
     {
         return 1;
+    }
+    
+    public boolean shouldSideBeRendered(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
+    {
+        int i1 = par1IBlockAccess.getBlockId(par2, par3, par4);
+        return !this.localFlag && i1 != 0 ? false : super.shouldSideBeRendered(par1IBlockAccess, par2, par3, par4, par5);
+    }
+    
+    public boolean renderAsNormalBlock()
+    {
+        return false;
     }
 }
--- cliffracerx/mods/cliffiesdecor/src/DecorModBlockTransp.java
+++ cliffracerx/mods/cliffiesdecor/src/DecorModBlockTransp.java
@@ -1,11 +1,15 @@
 package cliffracerx.mods.cliffiesdecor.src;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.IBlockAccess;
 
 public class DecorModBlockTransp extends Block
 {
     private String tex;
+    boolean localFlag=false;
     
     public DecorModBlockTransp(int id, Material material, String texture)
     {
@@ -17,5 +21,22 @@ public class DecorModBlockTransp extends Block
     public boolean isOpaqueCube()
     {
         return false;
+    }
+    
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+    
+    public boolean shouldSideBeRendered(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
+    {
+        int i1 = par1IBlockAccess.getBlockId(par2, par3, par4);
+        return !this.localFlag && i1 != 0 ? false : super.shouldSideBeRendered(par1IBlockAccess, par2, par3, par4, par5);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
     }
 }
